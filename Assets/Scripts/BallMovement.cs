4b85f3753668248459953a729995060578b38905
OVAL_PUBLIC <OVAL_PUBLIC@ULOVAL1-PC.sooner.net.ou.edu>
2020-02-18 14:15:31 -0600

    Made ball glow white. Used a bit of a hack, attaching a point light to the ball since the emissive material wasn't lighting nearby objects.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -7,7 +7,7 @@ public class BallMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<Rigidbody>().AddForce(new Vector3(10, 0, 0));
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 5));
     }
 
     // Update is called once per frame
