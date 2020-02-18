0a8e114949a83484317b83cff138e56fe854d23a
OVAL_PUBLIC <OVAL_PUBLIC@ULOVAL1-PC.sooner.net.ou.edu>
2020-02-18 14:45:53 -0600

    Began working on making tunnel walls pulse when the ball collides.

---
+++ Assets/Scripts/PulseOnBallHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseOnBallHit : MonoBehaviour {
+    public Material pulseMaterial;
+    public Material originalMaterial;
+
+    private void Start() {
+        this.originalMaterial = this.GetComponent<MeshRenderer>().GetMaterials();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.gameObject.tag == "Ball") {
+            this.GetComponent<MeshRenderer>().materials[0] = ;
+        }
+    }
+
+    void OnCollisionExit(Collision collision) {
+        if (collision.collider.gameObject.tag == "Ball") {
+
+        }
+    }
+}
