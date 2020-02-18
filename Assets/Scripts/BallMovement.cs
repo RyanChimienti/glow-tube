56102ebb26e0ba78f41fa65c9db1f2edfe725816
OVAL_PUBLIC <OVAL_PUBLIC@ULOVAL1-PC.sooner.net.ou.edu>
2020-02-18 13:22:26 -0600

    Fixed 'sticky' collisions by reducing bounce threshold from 2 to 0.01.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -7,7 +7,7 @@ public class BallMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 20));
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(10, 0, 0));
     }
 
     // Update is called once per frame
