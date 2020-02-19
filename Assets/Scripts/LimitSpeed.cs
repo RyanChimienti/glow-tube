e48b0dc0dd0cc41549d6abb13cf6698a95a73d64
OVAL_PUBLIC <OVAL_PUBLIC@ULOVAL1-PC.sooner.net.ou.edu>
2020-02-19 15:12:21 -0600

    Incorporated Unity's new (preview) package XR Interaction Toolkit. This greatly improved handling of the paddle, and should enable easy laser pointer UIs. The more realistic paddle physics led the ball to move unreasonably fast, so I gave it a speed limiter script.

---
+++ Assets/Scripts/LimitSpeed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitSpeed : MonoBehaviour
+{
+    public float maxSpeed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody rigidbody = this.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
+    }
+}
